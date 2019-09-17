373d6d91f63c76d0bf9f731ffecfc90fda7ab540
hmsajal <19496424+hmsajal@users.noreply.github.com>
2019-09-18 02:47:38 +0600

    reverted

--- App.js
+++ App.js
@@ -6,24 +6,6 @@ import Breath from './sources/breath'
 
 export default class App extends Component {
 
-  constructor(props){
-    super(props)
-      this.appAsyncGet()
-  }
-
-
-  appAsyncSet=async ()=>{
-     AsyncStorage.setItem('storageDuration','8')
-          AsyncStorage.setItem('storageSound','1')                       
-  }
-
- appAsyncGet=async ()=>{
-    const value = await AsyncStorage.getItem('storageDuration')        
-    if(value == null){
-      this.appAsyncSet()
-    }
-}
-
   render() {    
     return (      
       <Breath/>
